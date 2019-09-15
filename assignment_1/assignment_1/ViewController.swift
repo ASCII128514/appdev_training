90d62c909211a4a6fdc225f670c1db4cfcdd073b
Paul Lu <luhanfeng1106@sina.com>
2019-09-15 10:52:31 -0500

    demo app

--- assignment_1/assignment_1/ViewController.swift
+++ assignment_1/assignment_1/ViewController.swift
@@ -13,8 +13,55 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if (((UserDefaults.standard.object(forKey: "name"))) != nil) {
+            self.hasInput = true
+            check(hasInput: hasInput)
+            btn.setTitle("reset", for: .normal)
+        }
     }
-
+    
+    var hasInput = false
+    func check(hasInput: Bool) {
+        nameLabel.isHidden = hasInput
+        name.isHidden = hasInput
+        classYearLabel.isHidden = hasInput
+        classYear.isHidden = hasInput
+        name.text = ""
+        classYear.text = ""
+        let n = UserDefaults.standard.object(forKey: "name")
+        let y = UserDefaults.standard.object(forKey: "classYear")
+        welcomeMessage.text = "welcome back \(n ?? "")! class of \(y ?? "")"
+    }
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var name: UITextField!
+    
+    @IBOutlet weak var classYearLabel: UILabel!
+    
+    @IBOutlet weak var classYear: UITextField!
+    
+    @IBOutlet weak var welcomeMessage: UILabel!
+    
+    @IBOutlet weak var btn: UIButton!
+    
+    @IBAction func submit(_ sender: Any) {
+        if (!self.hasInput) {
+            let n = name.text ?? ""
+            let y = classYear.text ?? ""
+            UserDefaults.standard.set(n, forKey: "name")
+            UserDefaults.standard.set(y, forKey: "classYear")
+            self.hasInput = true
+            check(hasInput: hasInput)
+        } else {
+            // in this case it is reset
+            self.hasInput = false
+            check(hasInput: hasInput)
+            UserDefaults.standard.removeObject(forKey: "name")
+            UserDefaults.standard.removeObject(forKey: "classYear")
+            welcomeMessage.text = "welcome, plz enter your info"
+        }
+    }
 
 }
 
