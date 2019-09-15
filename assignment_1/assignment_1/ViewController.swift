b15ba38b4fe4757711575ea915f3ecafe6c5a486
Paul Lu <luhanfeng1106@sina.com>
2019-09-14 21:25:09 -0500

    assignment_1

---
+++ assignment_1/assignment_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment_1
+//
+//  Created by luhanfeng on 2019/9/14.
+//  Copyright © 2019 luhanfeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
